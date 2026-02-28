22253970578b24dc30f49b89b2698fe774cc91ff
suguna-kante <kantesuguna80@gmail.com>
2026-02-28 13:05:38 +0530

    Time: 23 ms (94.63%) | Memory: 7.9 MB (89.26%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD =1e9+7;
+        long long r=0;
+        int bits=0;
+        for(int i=1;i<=n;i++){
+            if((i&(i-1))==0) bits++;
+            r=((r<<bits)|i)%MOD;
+
+        }
+        return r;
+        
+    }
+};
