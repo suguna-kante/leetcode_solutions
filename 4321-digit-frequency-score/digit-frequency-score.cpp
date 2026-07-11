37d19797ad79a61f6483cd9a047359357e656108
suguna-kante <kantesuguna80@gmail.com>
2026-07-11 09:11:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (80.93%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int s=0;
+        while(n>0){
+            s+=n%10;
+            n/=10;
+        }
+        return s;
+        
+    }
+};
