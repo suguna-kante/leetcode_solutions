ba8a8417e1255a3f9f621805d68d2e690922d4bc
suguna-kante <kantesuguna80@gmail.com>
2026-07-15 10:40:45 +0530

    Time: 184 ms (50.02%) | Memory: 12.8 MB (39.09%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<2) return 0;
+        vector<bool>p(n+1,true);
+        for(int i=2;i<=sqrt(n);i++){
+            if(p[i]){
+                for(int j=i*i;j<n;j+=i){
+                    p[j]=false;
+                }
+            }
+        }
+        int c=0;
+        for(int i=2;i<n;i++){
+            if(p[i])
+                c++;
+        }
+        return c;
+        
+    }
+};
