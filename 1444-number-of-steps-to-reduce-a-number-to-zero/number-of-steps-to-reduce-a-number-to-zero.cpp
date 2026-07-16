c9bb691e4506e5e0ce86bc2ef8ad34a91509ba9d
suguna-kante <kantesuguna80@gmail.com>
2026-07-16 14:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.13%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int c=0;
+        while(num>0){
+            c++;
+            if(num%2==0){
+                num/=2;
+               
+            }else{
+                num=num-1;
+            }
+        }
+       return c;
+        
+    }
+};
