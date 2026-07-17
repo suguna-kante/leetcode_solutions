a2e5f214e379614ef1f49500fb0a879b9304cbd5
suguna-kante <kantesuguna80@gmail.com>
2026-07-17 15:27:06 +0530

    Time: 3 ms (7.36%) | Memory: 8.1 MB (50.77%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int c=0;
+        int a=0;
         while(n>0){
             n/=5;
-            c+=n;
+            a+=n;
         }
-        return c;
+        return a;
+
+
         
     }
 };
